a044ffbb988b19a8feb3a14f07323e7bceb13753
YYZ <YYZ>
2020-04-04 18:10:43 +0800

    lesson:class

--- student_teacher.py
+++ student_teacher.py
@@ -2,7 +2,7 @@ class Person(object):
     """
     Returen person name with given object
     """
-    def _init_(self,name):
+    def __init__(self,name):
         self.name=name
 
     def get_details(self):
@@ -14,9 +14,24 @@ class Student(Person):
     """
     Return student object, using name,branch,year parameters
     """
-    def _init_(self,name,branch,year):
-        Person._init_(self,name)
+    def __init__(self,name,branch,year):
+        Person.__init__(self,name)
         self.branch=branch
         self.year=year
     def get_details(self):
-        retuen "{} studies {} and is in {} year.".format(self.name)
+        return "{} studies {} and is in {} year.".format(self.name,self.branch,self.year)
+
+class Teacher(Person):
+    def __init__(self,name,papers):
+        Person.__init__(self,name)
+        self.papers=papers
+    def get_details(self):
+        return "{} teaches {}".format(self.name,','.join(self.papers))
+
+person1=Person('Sachin')
+student1=Student('Kusha1','CSE',2005)
+teacher1=Teacher('Prashad',['C','C++'])
+
+print(person1.get_details())
+print(student1.get_details())
+print(teacher1.get_details())
