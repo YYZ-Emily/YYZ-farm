00df0785a7db8933dfcb98085cf3aeb40f9dadcd
YYZ <YYZ>
2020-04-06 16:17:27 +0800

    lesson:class-requests

--- download.py
+++ download.py
@@ -1,14 +1,22 @@
 import requests
-
+import os
 def download(url):
+    filename=url.split('/')[-1]
+    if os.path.exists(filename):
+        print('The file is already exist.')
+        return
     try:
         req=requests.get(url)
     except requests.exceptions.MissingSchema:
         print('Invalid URL "{}"'.format(url))
         return
-    if req.status_code==403
+    if req.status_code==403:
         print('You do not have the authority to access this page.')
         return
     filename=url.split('/')[-1]
     with open(filename,'w') as fobj:
         fobj.write(req.content.decode('utf-8'))
+
+if __name__=='__main__':
+    url=input('Enterd a URL:')
+    download(url)
